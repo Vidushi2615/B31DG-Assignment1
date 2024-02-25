1f0a21d297c0fdaf7395ba61c9faaae098de19b3
Vidushi2615 <161159911+Vidushi2615@users.noreply.github.com>
2024-02-25 19:26:19 +0000

    Update Test_Assignment1.ino

--- Test_Assignment1.ino
+++ Test_Assignment1.ino
@@ -14,14 +14,6 @@
 #define u 21
 #define p 16
 
-//Calculate the Parameters: 5 letters of surname "Nadup"
-int A = n * 100; // parameter 'a' calculated as n*100
-int B = a * 100; // parameter 'b' calculated as a*100
-int C = d + 4; // parameter 'c' calculated as d+4
-int D = u * 500; // parameter 'd' calculated as u*500
-int M = ( p % 4 ) + 1;// mode is calculated as rem(p/4) + 1 = rem(16/4) + 1 = 1. Hence we use mode 1
-int R = C - 3; // parameter used in mode '1' where we reduce the pulses by 3, r = c-3
-
 //Variables to read switch values and determine the case
 int sw1 = 0, sw2 = 0; //Switch 1 and Switch 2
 int temp = 0;
@@ -29,8 +21,8 @@ int temp = 0;
 void setup() {
   Serial.begin(9600);
 
-  pinMode(s1, INPUT_PULLDOWN); //setting switch 1 pin as INPUT with pull down resistor configuration
-  pinMode(s2, INPUT_PULLDOWN); //setting switch 1 pin as INPUT with pull down resistor configuration
+  pinMode(s1, INPUT_PULLDOWN); //setting switch 1 pin as INPUT with pull-down resistor configuration
+  pinMode(s2, INPUT_PULLDOWN); //setting switch 1 pin as INPUT with pull-down resistor configuration
   pinMode(sigA, OUTPUT);// setting sigA pin(LED 1/signal A) as OUTPUT
   pinMode(sigB, OUTPUT);// setting sigB pin(LED 2/signal B) as OUTPUT
 
@@ -38,36 +30,31 @@ void setup() {
 }
 
 void loop() {
+  //Calculate the Parameters: 5 letters of the surname "Nadup"
+  int A = n * 100; // parameter 'a' calculated as n*100
+  int B = a * 100; // parameter 'b' calculated as a*100
+  int C = d + 4; // parameter 'c' calculated as d+4
+  int D = u * 500; // parameter 'd' calculated as u*500
+  int M = ( p % 4 ) + 1;// mode is calculated as rem(p/4) + 1 = rem(16/4) + 1 = 1. Hence we use mode 1
+  int R = C - 3; // parameter used in mode '1' where we reduce the pulses by 3, r = c-3
+
   sw1 = digitalRead(s1); // reading from Switch 1 (s1) and storing it in sw1
   sw2 = digitalRead(s2); // reading from Switch 2 (s2) and storing it in sw2
   
   if(sw1 == HIGH)         // using if else to determine the case
   {
-    temp = 1;
+    digitalWrite(sigA, LOW);
+    digitalWrite(sigB, LOW);
   }
 
   else{
+
+    digitalWrite(sigB, HIGH);
+    delay(50);
+    digitalWrite(sigB, LOW);
+    
     if(sw2 == LOW)
     {
-      temp = 2;
-    }
-    else
-    {
-      temp = 3;
-    }
-  }
-
-  switch (temp) 
-  {
-    case 1:
-      digitalWrite(sigA, LOW);
-      digitalWrite(sigB, LOW);
-
-    case 2:
-      digitalWrite(sigB, HIGH);
-      delay(50);
-      digitalWrite(sigB, LOW);
-
       for(int i = 0 ; i < C ; i++ )
       {
         digitalWrite(sigA, HIGH);
@@ -76,13 +63,9 @@ void loop() {
         delay(B);
         A = A+50;
       }
-      delay(D);
-
-    case 3: 
-      digitalWrite(sigB, HIGH);
-      delay(50);
-      digitalWrite(sigB, LOW);
-
+    }
+    else
+    {
       for(int i = 0 ; i < R ; i++ )
       {
         digitalWrite(sigA, HIGH);
@@ -91,6 +74,9 @@ void loop() {
         delay(B);
         A = A+50;
       }
-      delay(D);
+    }
+    
+    delay(D);
+  
   }
 }
